14f0d3bddae4a88a9d9c5ce5a0568a14a8ef5e1d
DESKTOP-R8KP20E\Profesor <csi23-agarera@colegioaltair.net>
2024-01-16 12:44:02 +0100

    Agregar archivos de proyecto.

---
+++ ejercicio1.repaso/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ejercicio1.repaso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el numero de minutos trabajados en un mes:");
+            int numeroMinutos = Convert.ToInt32(Console.ReadLine());
+
+            int horasDiaTrabajo = 8;
+
+            int dia = numeroMinutos / (horasDiaTrabajo * 60);
+            int horas = numeroMinutos % (horasDiaTrabajo * 60) / 60;
+            int minutos = numeroMinutos % 60;
+
+            int dias;
+            int minuto;
+            int hora;
+
+            if (dia > 0)
+            {
+                Console.WriteLine(dia + "días");
+            }
+            if (horas > 0)
+            {
+                Console.WriteLine(horas + "horas");
+            }
+            if (minutos > 0)
+            {
+                Console.WriteLine(minutos + "minutos");
+            }
+            
+            Console.WriteLine(dia + " " + "días" + " " + horas + " " + "horas" + " " + minutos + " " + "minutos");
+
+            
+        }
+    }
+}
